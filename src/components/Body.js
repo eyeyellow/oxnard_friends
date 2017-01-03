69f71a370a9dd2a5912510569429ac54e067ac93
Frank Aiello <frankvaiello@gmail.com>
2017-01-02 17:39:52 -0800

    adds text to navbar and images to body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import insideBookstore from '../images/inside-bookstore.jpg';
+import libraryFriends from '../images/library-friends.jpg';
+import outsideBookstore from '../images/outside-bookstore.jpg';
+import outsideLibrary from '../images/outside-library.jpg';
 
 class Body extends Component {
   render() {
     return (
       <div className="Body">
-        <p>Body</p>
+      <img src={insideBookstore} alt="insideBookstore" className="inside-bookstore" />
+      <img src={libraryFriends} alt="libraryFriends" className="library-friends" />
+      <img src={outsideLibrary} alt="outsideLibrary" className="outside-library" />
+      <img src={outsideBookstore} alt="outsideBookstore" className="outside-bookstore" />
       </div>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,15 @@ class NavBar extends Component {
   render() {
     return (
       <div className="NavBar">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About</li>
+          <li>Support Us</li>
+          <li>Volunteer</li>
+          <li>Events</li>
+          <li>Bookstore</li>
+          <li>Location</li>
+        </ul>
       </div>
     );
   }
