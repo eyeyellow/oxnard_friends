e047ac76cc06df7d8daaebd088e82d73b5e185fe
Frank Aiello <frankvaiello@gmail.com>
2017-01-12 09:19:40 -0800

    adds positioning for background bookshelf image

--- src/components/containers/IconsContainer.js
+++ src/components/containers/IconsContainer.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import IconItem from '../../components/IconItem';
 import { items } from '../../../public/data';
+import About from '../../components/About';
 
 class IconsContainer extends Component {
   render() {
     return (
       <div className="icons-container">
+        <About className="about" />
         <table className="icons-table">
           <tbody>
-            <tr>
-              <td colSpan="4" >
-                <div className="divider-cell">
-                </div>
-              </td>
-            </tr>
             <tr>
               {items.map(function(item) {
                 return <IconItem iconPath={item.svgIcon} key={item.id} text={item.title} />
